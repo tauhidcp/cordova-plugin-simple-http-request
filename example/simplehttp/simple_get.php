4d5ad034b0fb07c81f93980195f0d697edf8ffe6
Ahmad Tauhid <ahmad.tauhid.cp@gmail.com>
2023-02-23 14:28:28 +0800

    Update simple_get.php

--- example/simplehttp/simple_get.php
+++ example/simplehttp/simple_get.php
@@ -3,4 +3,8 @@
 echo "Balasan dari Server"; // Simple Respon From Server
 //echo "Nama Anda ". $_GET['user']; // Get Parameter
 
+// JSON Output
+//$data = array("nama"=>"Ahmad Tauhid", "nohp"=>"081907558xxx", "alamat"=>"Lombok NTB");
+//echo json_encode($data);
+
 ?>
